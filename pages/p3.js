2bbe40e1834edf396b79fa9435a3a07bb793ad5b
Robin Millette <robin@millette.info>
2019-10-08 18:42:54 -0400

    Use MDX style guide component.

--- pages/p3.js
+++ pages/p3.js
@@ -1,53 +1,65 @@
 // npm
 import Link from 'next/link'
-import { Styled } from 'theme-ui'
+import { Flex, Box, Styled } from 'theme-ui'
 import {
-  // ThemeCard,
   TypeScale,
   TypeStyle,
   ColorPalette,
-  // HeadingStyle,
-  // FontFamily,
 } from '@theme-ui/style-guide'
 
 // self
 import LightSwitch from '../components/light-switch'
-
-/*
-HeadingStyle:
-<HeadingStyle />
-
-ThemeCard:
-<ThemeCard />
-
-// Renders no output, breaks react
-// <FontFamily />
-*/
+import StyleGuide from '../components/style-guide.mdx'
 
 export default () =>
   <div>
     <Styled.h1>Style Guide</Styled.h1>
-    <LightSwitch />
     <p>go <Link href="/"><a>Home</a></Link></p>
     <p>go <Link href="/p2"><a>Page 2</a></Link></p>
+    <Flex>
+      <Box>
+        <Styled.p>
+        I am a styled paragraph. I am a paragraph. I am a <b>strong paragraph</b>. I am a paragraph. I am a paragraph. I am a paragraph. I am a paragraph. I am a paragraph. I am a <i>light paragraph</i>. I am a paragraph. I am a paragraph. I am a paragraph. I am a paragraph.
+        </Styled.p>
 
-    ColorPalette:
-    <ColorPalette />
+        <p>
+        I am a paragraph. I am a paragraph. I am a <b>strong paragraph</b>. I am a paragraph. I am a paragraph. I am a paragraph. I am a paragraph. I am a paragraph. I am a <i>light paragraph</i>. I am a paragraph. I am a paragraph. I am a paragraph. I am a paragraph.
+        </p>
 
-    TypeScale:
-    <TypeScale />
 
-    TypeStyle 1:
-    <TypeStyle
-      fontFamily='heading'
-      fontWeight='heading'
-      lineHeight='heading'
-    />
+        <Styled.blockquote>
+        I am a styled quote. I am a quote. I am a <b>strong quote</b>. I am a quote. I am a quote. I am a quote. I am a quote. I am a quote. I am a <i>light quote</i>. I am a quote. I am a quote. I am a quote. I am a quote.
+        </Styled.blockquote>
 
-    TypeStyle 2:
-    <TypeStyle
-      fontFamily='body'
-      fontWeight='body'
-      lineHeight='body'
-    />
+        <blockquote>
+        I am a quote. I am a quote. I am a <b>strong quote</b>. I am a quote. I am a quote. I am a quote. I am a quote. I am a quote. I am a <i>light quote</i>. I am a quote. I am a quote. I am a quote. I am a quote.
+        </blockquote>
+
+
+        ColorPalette:
+        <ColorPalette />
+
+        TypeScale:
+        <TypeScale />
+
+        TypeStyle 1:
+        <TypeStyle
+          fontFamily='heading'
+          fontWeight='heading'
+          lineHeight='heading'
+        />
+
+        TypeStyle 2:
+        <TypeStyle
+          fontFamily='body'
+          fontWeight='body'
+          lineHeight='body'
+        />
+      </Box>
+
+      <Box>
+        <LightSwitch />
+        <StyleGuide />
+      </Box>
+    </Flex>
   </div>
