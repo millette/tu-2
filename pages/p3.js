a36ca8df5b82f943eefa1cfde7fd844c2b1344cd
Robin Millette <robin@millette.info>
2019-10-08 03:47:19 -0400

    Use @theme-ui/style-guide.

---
+++ pages/p3.js
@@ -0,0 +1,23 @@
+import { Styled } from 'theme-ui'
+import {
+  TypeScale,
+  TypeStyle,
+  ColorPalette,
+} from '@theme-ui/style-guide'
+
+export default () =>
+  <>
+    <Styled.h1>Style Guide</Styled.h1>
+    <ColorPalette />
+    <TypeScale />
+    <TypeStyle
+      fontFamily='heading'
+      fontWeight='heading'
+      lineHeight='heading'
+    />
+    <TypeStyle
+      fontFamily='body'
+      fontWeight='body'
+      lineHeight='body'
+    />
+  </>
