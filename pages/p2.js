b862d2ea3039a25cccbb3c7245efafd7b2b54a53
Robin Millette <robin@millette.info>
2019-10-08 01:36:40 -0400

    Add page 2.

---
+++ pages/p2.js
@@ -0,0 +1,23 @@
+/** @jsx jsx */
+
+// npm
+import Link from 'next/link'
+import { jsx } from 'theme-ui'
+import { useColorMode } from 'theme-ui'
+
+export default (props) => {
+  const [colorMode, setColorMode] = useColorMode()
+
+  return (
+    <div>
+      <h1>Page 2!!</h1>
+      <button
+        onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}>
+        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+      </button>
+
+      <p>go <Link href="/"><a>Home</a></Link></p>
+      <button sx={{color: 'white', bg: 'primary'}}>A button</button>
+    </div>
+  )
+}
