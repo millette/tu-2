bb00786f19b5a49145b44aa54c2f01861e0d5318
Robin Millette <robin@millette.info>
2019-10-08 04:49:47 -0400

    Try out more style guide components.

--- pages/p3.js
+++ pages/p3.js
@@ -1,20 +1,42 @@
 import { Styled } from 'theme-ui'
 import {
+  // ThemeCard,
   TypeScale,
   TypeStyle,
   ColorPalette,
+  // HeadingStyle,
+  // FontFamily,
 } from '@theme-ui/style-guide'
+
+/*
+HeadingStyle:
+<HeadingStyle />
+
+ThemeCard:
+<ThemeCard />
+
+// Renders no output, breaks react
+// <FontFamily />
+*/
 
 export default () =>
   <>
     <Styled.h1>Style Guide</Styled.h1>
+
+    ColorPalette:
     <ColorPalette />
+
+    TypeScale:
     <TypeScale />
+
+    TypeStyle 1:
     <TypeStyle
       fontFamily='heading'
       fontWeight='heading'
       lineHeight='heading'
     />
+
+    TypeStyle 2:
     <TypeStyle
       fontFamily='body'
       fontWeight='body'
