30b207942cd2611c8159787ffda45ce5feaa6cb2
Robin Millette <robin@millette.info>
2019-10-09 02:20:36 -0400

    Touchups.

--- components/light-switch.js
+++ components/light-switch.js
@@ -6,7 +6,7 @@ import { jsx, useColorMode } from 'theme-ui'
 export default () => {
   const [colorMode, setColorMode] = useColorMode()
   return (
-    <button onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}>
+    <button sx={{variant: 'primary'}} onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}>
       Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
     </button>
   )
