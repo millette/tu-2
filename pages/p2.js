822aafba62b87e751b8e76f8687e5ed6eef2eea6
Robin Millette <robin@millette.info>
2019-10-26 18:54:37 -0400

    Simplify page 2.

--- pages/p2.js
+++ pages/p2.js
@@ -2,29 +2,14 @@
 
 // npm
 import Link from 'next/link'
-// import { jsx, useColorMode } from 'theme-ui'
 import { jsx } from 'theme-ui'
 
-// self
-import LightSwitch from '../components/light-switch'
-
-export default () => {
-  // const [colorMode, setColorMode] = useColorMode()
-
-/*
-<button
-  onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}>
-  Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-</button>
-*/
-
-  return (
-    <div>
-      <h1>Page 2!!</h1>
-      <LightSwitch />
-      <p>go <Link href="/"><a>Home</a></Link></p>
-      <p>go <Link href="/p3"><a>Page 3</a></Link></p>
-      <button sx={{color: 'white', bg: 'primary'}}>A button</button>
-    </div>
-  )
-}
+export default () => (
+  <div>
+    <h1>Page 2!!</h1>
+    <p>go <Link href="/"><a>Home</a></Link></p>
+    <p>go <Link href="/theme"><a>Theme</a></Link></p>
+    <p>go <Link href="/p3"><a>Page 3</a></Link></p>
+    <button sx={{color: 'white', bg: 'primary'}}>A button</button>
+  </div>
+)
