c00bd0786f06350b252a3c52e1c194090ce3ead5
Robin Millette <robin@millette.info>
2019-10-26 18:52:15 -0400

    Modify theme support.

--- pages/_app.js
+++ pages/_app.js
@@ -1,63 +1,34 @@
 /** @jsx jsx */
 
 // npm
+// import React from "react"
 import App from "next/app"
 import Link from 'next/link'
-// import { MDXProvider } from "@mdx-js/react"
-// import { jsx, useColorMode, ThemeProvider, Styled, ColorMode, Flex, Box, Layout, Header, Main, Container, Footer, InitializeColorMode } from "theme-ui"
-// import { jsx, ThemeProvider, Styled, ColorMode, Layout } from "theme-ui"
 import { jsx, ThemeProvider, Styled, ColorMode } from "theme-ui"
-import * as themes from "@theme-ui/presets"
+import { bootstrap as theme } from "@theme-ui/presets"
 
-// self
-import darkenTheme from '../darken-theme'
-
-// import theme from '../theme'
-
-// const components = { Flex, Box, Layout, Header, Main, Container, Footer }
 const components = {
-  a: ({ children, href }) => <Link href={href}><a>{children}</a></Link>
+  a: ({ children, href }) => <Link href={href}><Styled.a>{children}</Styled.a></Link>
 }
 
-// <InitializeColorMode />
-// <ColorMode />
-
-// <Styled.root>
-// </Styled.root>
-
-
 class MyApp extends App {
-  state = { selectedTheme: 'base' }
-  // <Component {...pageProps} changeTheme={this.setState.bind(this)} />
-  // {this.state.selectedTheme}
+  constructor (props) {
+    super(props)
+    this.state = { theme }
+    this.setTheme = (theme) => this.setState({ theme })
+  }
 
-// <Layout>
-// </Layout>
-
-render() {
+  render() {
     const { Component, pageProps } = this.props
     return (
-      <ThemeProvider theme={darkenTheme(themes[this.state.selectedTheme])} components={components}>
+      <ThemeProvider components={components} theme={this.state.theme}>
         <ColorMode />
         <Styled.root>
-          <Component {...pageProps} selectedTheme={this.state.selectedTheme} themes={Object.keys(themes)} changeTheme={this.setState.bind(this)} />
+          <Component {...pageProps} updateTheme={this.setTheme} />
         </Styled.root>
       </ThemeProvider>
     )
   }
 }
-
-
-// <MDXProvider components={components}>
-// </MDXProvider>
-
-/*
-<button
-  onClick={e => {
-    setColorMode(colorMode === 'light' ? 'dark' : 'light')
-  }}>
-  Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-</button>
-*/
 
 export default MyApp
