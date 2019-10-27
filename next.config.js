699af96efe0db439a783a71486954097c441ec07
Robin Millette <robin@millette.info>
2019-10-27 16:40:46 -0400

    Configure url-loader to support svg (in css).

--- next.config.js
+++ next.config.js
@@ -1,25 +1,23 @@
 "use strict"
 
 const withMDX = require("@next/mdx")()
-// const withCSS = require("@zeit/next-css")
-const withCSS = (x) => x
+const withCSS = require("@zeit/next-css")
+// const withCSS = (x) => x
 
 module.exports = withCSS(
   withMDX({
     pageExtensions: ["js", "mdx"],
-    /*
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
         use: {
-          loader: 'url-loader',
+          loader: "url-loader",
           options: {
-            limit: 100000
-          }
-        }
+            limit: 100000,
+          },
+        },
       })
       return config
     },
-    */
   })
 )
