7496f324a19d7218825933432321dfe34fef57cb
Robin Millette <robin@millette.info>
2019-10-27 17:54:03 -0400

    Cleanup.

--- components/ed-2.js
+++ components/ed-2.js
@@ -3,13 +3,12 @@ import dynamic from "next/dynamic"
 import "jsoneditor-react/es/editor.css"
 
 const JsonEditor = dynamic(
-  () => import("jsoneditor-react").then(({ JsonEditor }) => JsonEditor)
-  // { ssr: false }
+  () => import("jsoneditor-react").then(({ JsonEditor }) => JsonEditor),
+  { ssr: false }
 )
 
-const Editor = ({ onChange, json }) => {
-  console.log("JsonEditor", typeof JsonEditor, JsonEditor)
-  return <JsonEditor value={json} onChange={onChange} />
-}
+const Editor = ({ onChange, json }) => (
+  <JsonEditor value={json} onChange={onChange} />
+)
 
 export default Editor
