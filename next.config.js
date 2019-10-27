05efb376a691d2437753c81d05d8a06fc469155b
Robin Millette <robin@millette.info>
2019-10-27 01:14:41 -0400

    Add css support.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 "use strict"
 
 const withMDX = require("@next/mdx")()
+const withCSS = require("@zeit/next-css")
 
-module.exports = withMDX({
-  pageExtensions: ["js", "mdx"],
-})
+module.exports = withCSS(
+  withMDX({
+    pageExtensions: ["js", "mdx"],
+  })
+)
