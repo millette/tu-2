fcc799e79349a1caff907785fb93e6521bc95ed2
Robin Millette <robin@millette.info>
2019-10-27 01:04:48 -0400

    Only show close button if there's a handler.

--- components/demo-modal.js
+++ components/demo-modal.js
@@ -6,11 +6,11 @@ import { jsx, Styled } from "theme-ui"
 // self
 import Modal from "./modal"
 
-const DemoModal = ({ shown = true, close = () => false }) =>
+const DemoModal = ({ shown = true, close = false }) =>
   shown && (
     <Modal>
       <Styled.p>
-        <button onClick={close}>[X]</button>
+        {close && <button onClick={close}>[X]</button>}
         Hello Modal
       </Styled.p>
     </Modal>
