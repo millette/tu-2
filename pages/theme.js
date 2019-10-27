2eba6420f724c6aabd9b2e28944a94fd795b6b4e
Robin Millette <robin@millette.info>
2019-10-26 20:26:56 -0400

    Prettier theme page.

--- pages/theme.js
+++ pages/theme.js
@@ -1,13 +1,13 @@
 /** @jsx jsx */
 
 // npm
-import Link from 'next/link'
-import { jsx, useThemeUI, Styled } from 'theme-ui'
+import Link from "next/link"
+import { jsx, useThemeUI, Styled } from "theme-ui"
 
 // self
-import Editor from '../components/ed'
+import Editor from "../components/ed"
 
-export default ({ updateTheme } ) => {
+export default ({ updateTheme }) => {
   const { theme } = useThemeUI()
 
   const clicky = () => {
@@ -18,12 +18,27 @@ export default ({ updateTheme } ) => {
   return (
     <div>
       <Styled.h1>Page 2!!</Styled.h1>
-      <Styled.p>go <Link href="/"><a>Home</a></Link></Styled.p>
-      <Styled.p>go <Link href="/p3"><a>Page 3</a></Link></Styled.p>
-      <Styled.p>go <Link href="/p4"><a>Page 4</a></Link></Styled.p>
+      <Styled.p>
+        go{" "}
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </Styled.p>
+      <Styled.p>
+        go{" "}
+        <Link href="/p3">
+          <a>Page 3</a>
+        </Link>
+      </Styled.p>
+      <Styled.p>
+        go{" "}
+        <Link href="/p4">
+          <a>Page 4</a>
+        </Link>
+      </Styled.p>
       <button onClick={clicky}>Clicky</button>
       <Editor onChange={updateTheme} json={theme} />
-      <button sx={{color: 'white', bg: 'primary'}}>A button</button>
+      <button sx={{ color: "white", bg: "primary" }}>A button</button>
     </div>
   )
 }
