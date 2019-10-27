1788a495c9a62986e8fe4a6513fb85bd08722f8e
Robin Millette <robin@millette.info>
2019-10-26 20:26:38 -0400

    Prettier page 3.

--- pages/p3.js
+++ pages/p3.js
@@ -1,62 +1,73 @@
 /** @jsx jsx */
 
 // npm
-import Link from 'next/link'
-import { jsx, Flex, Box, Styled } from 'theme-ui'
-import {
-  TypeScale,
-  TypeStyle,
-  ColorPalette,
-} from '@theme-ui/style-guide'
+import Link from "next/link"
+import { jsx, Flex, Box, Styled } from "theme-ui"
+import { TypeScale, TypeStyle, ColorPalette } from "@theme-ui/style-guide"
 
 // self
-import StyleGuide from '../components/style-guide.mdx'
+import StyleGuide from "../components/style-guide.mdx"
 
 export default () => (
   <div>
     <Styled.h1>Style Guide</Styled.h1>
-    <p>go <Link href="/"><a>Home</a></Link></p>
-    <p>go <Link href="/theme"><a>Theme</a></Link></p>
-    <p>go <Link href="/p2"><a>Page 2</a></Link></p>
+    <p>
+      go{" "}
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </p>
+    <p>
+      go{" "}
+      <Link href="/theme">
+        <a>Theme</a>
+      </Link>
+    </p>
+    <p>
+      go{" "}
+      <Link href="/p2">
+        <a>Page 2</a>
+      </Link>
+    </p>
     <Flex>
       <Box>
         <Styled.p>
-        I am a styled paragraph. I am a paragraph. I am a <b>strong paragraph</b>. I am a paragraph. I am a paragraph. I am a paragraph. I am a paragraph. I am a paragraph. I am a <i>light paragraph</i>. I am a paragraph. I am a paragraph. I am a paragraph. I am a paragraph.
+          I am a styled paragraph. I am a paragraph. I am a{" "}
+          <b>strong paragraph</b>. I am a paragraph. I am a paragraph. I am a
+          paragraph. I am a paragraph. I am a paragraph. I am a{" "}
+          <i>light paragraph</i>. I am a paragraph. I am a paragraph. I am a
+          paragraph. I am a paragraph.
         </Styled.p>
-
         <p>
-        I am a paragraph. I am a paragraph. I am a <b>strong paragraph</b>. I am a paragraph. I am a paragraph. I am a paragraph. I am a paragraph. I am a paragraph. I am a <i>light paragraph</i>. I am a paragraph. I am a paragraph. I am a paragraph. I am a paragraph.
+          I am a paragraph. I am a paragraph. I am a <b>strong paragraph</b>. I
+          am a paragraph. I am a paragraph. I am a paragraph. I am a paragraph.
+          I am a paragraph. I am a <i>light paragraph</i>. I am a paragraph. I
+          am a paragraph. I am a paragraph. I am a paragraph.
         </p>
-
-
         <Styled.blockquote>
-        I am a styled quote. I am a quote. I am a <b>strong quote</b>. I am a quote. I am a quote. I am a quote. I am a quote. I am a quote. I am a <i>light quote</i>. I am a quote. I am a quote. I am a quote. I am a quote.
+          I am a styled quote. I am a quote. I am a <b>strong quote</b>. I am a
+          quote. I am a quote. I am a quote. I am a quote. I am a quote. I am a{" "}
+          <i>light quote</i>. I am a quote. I am a quote. I am a quote. I am a
+          quote.
         </Styled.blockquote>
-
         <blockquote>
-        I am a quote. I am a quote. I am a <b>strong quote</b>. I am a quote. I am a quote. I am a quote. I am a quote. I am a quote. I am a <i>light quote</i>. I am a quote. I am a quote. I am a quote. I am a quote.
+          I am a quote. I am a quote. I am a <b>strong quote</b>. I am a quote.
+          I am a quote. I am a quote. I am a quote. I am a quote. I am a{" "}
+          <i>light quote</i>. I am a quote. I am a quote. I am a quote. I am a
+          quote.
         </blockquote>
-
-
         ColorPalette:
         <ColorPalette />
-
         TypeScale:
         <TypeScale />
-
         TypeStyle 1:
         <TypeStyle
-          fontFamily='heading'
-          fontWeight='heading'
-          lineHeight='heading'
+          fontFamily="heading"
+          fontWeight="heading"
+          lineHeight="heading"
         />
-
         TypeStyle 2:
-        <TypeStyle
-          fontFamily='body'
-          fontWeight='body'
-          lineHeight='body'
-        />
+        <TypeStyle fontFamily="body" fontWeight="body" lineHeight="body" />
       </Box>
 
       <Box>
