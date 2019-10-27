1285946b053d589919726010cf1763d29d2aec3f
Robin Millette <robin@millette.info>
2019-10-26 20:26:29 -0400

    Prettier page 2.

--- pages/p2.js
+++ pages/p2.js
@@ -1,15 +1,30 @@
 /** @jsx jsx */
 
 // npm
-import Link from 'next/link'
-import { jsx } from 'theme-ui'
+import Link from "next/link"
+import { jsx } from "theme-ui"
 
 export default () => (
   <div>
     <h1>Page 2!!</h1>
-    <p>go <Link href="/"><a>Home</a></Link></p>
-    <p>go <Link href="/theme"><a>Theme</a></Link></p>
-    <p>go <Link href="/p3"><a>Page 3</a></Link></p>
-    <button sx={{color: 'white', bg: 'primary'}}>A button</button>
+    <p>
+      go{" "}
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </p>
+    <p>
+      go{" "}
+      <Link href="/theme">
+        <a>Theme</a>
+      </Link>
+    </p>
+    <p>
+      go{" "}
+      <Link href="/p3">
+        <a>Page 3</a>
+      </Link>
+    </p>
+    <button sx={{ color: "white", bg: "primary" }}>A button</button>
   </div>
 )
