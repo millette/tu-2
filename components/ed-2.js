af38f92cbf3046cd44e362a903d90516fdfbf3fb
Robin Millette <robin@millette.info>
2019-10-27 16:41:20 -0400

    Use dynamic import for jsoneditor.

--- components/ed-2.js
+++ components/ed-2.js
@@ -1,11 +1,15 @@
-/** @jsx jsx */
-
 // npm
-import { jsx } from "theme-ui"
-import { JsonEditor } from "jsoneditor-react"
+import dynamic from "next/dynamic"
+import "jsoneditor-react/es/editor.css"
 
-const Editor = ({ onChange, json }) => (
-  <JsonEditor value={json} onChange={onChange} />
+const JsonEditor = dynamic(
+  () => import("jsoneditor-react").then(({ JsonEditor }) => JsonEditor)
+  // { ssr: false }
 )
+
+const Editor = ({ onChange, json }) => {
+  console.log("JsonEditor", typeof JsonEditor, JsonEditor)
+  return <JsonEditor value={json} onChange={onChange} />
+}
 
 export default Editor
