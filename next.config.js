ce4755bbc30e50848ce647d9cc143bdc0e8ef05b
Robin Millette <robin@millette.info>
2019-10-27 19:26:57 -0400

    Unconfigure css support.

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 "use strict"
 
 const withMDX = require("@next/mdx")()
-const withCSS = require("@zeit/next-css")
 
-module.exports = withCSS(
-  withMDX({
-    pageExtensions: ["js", "mdx"],
-    webpack: (config, options) => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 100000,
-          },
-        },
-      })
-      return config
-    },
-  })
-)
+module.exports = withMDX({
+  pageExtensions: ["js", "mdx"],
+})
