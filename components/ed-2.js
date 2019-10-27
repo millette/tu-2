68442107f7138a7d935fe03ba34856d8669a1aff
Robin Millette <robin@millette.info>
2019-10-27 19:04:48 -0400

    Use jsoneditor directly.

--- components/ed-2.js
+++
@@ -1,7 +0,0 @@
-// npm
-import { JsonEditor } from "jsoneditor-react"
-// import "jsoneditor-react/es/editor.css"
-
-const Editor = (props) => <JsonEditor {...props} />
-
-export default Editor
--- components/ed-modal.js
+++ components/ed-modal.js
@@ -1,12 +1,22 @@
 // npm
 import dynamic from "next/dynamic"
+// import Head from "next/head"
+
+// import "jsoneditor-react/es/editor.css"
 
 // self
 import Modal from "./modal"
 
 const DemoModal = ({ onChange, json, shown = true, close = false }) => {
   if (!shown) return null
-  const Editor = dynamic(() => import("./ed-2"), { ssr: false })
+  // const Xcss = dynamic(() => import("jsoneditor-react/es/editor.css"))
+  // console.log('TYPEOF X', Xcss.contextType, Object.keys(Xcss))
+  // const Editor = dynamic(() => import("./ed-2"), { ssr: false })
+  // Investigate: doesn't work directly unless wrapped in ed-2.js
+  const Editor = dynamic(
+    () => import("jsoneditor-react").then((mod) => mod.JsonEditor),
+    { ssr: false }
+  )
 
   return (
     <Modal>
