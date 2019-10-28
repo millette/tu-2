99c32a21f9b64ebe13e19fc2b6b78986686baa86
Robin Millette <robin@millette.info>
2019-10-27 20:08:47 -0400

    'Loading' string touchups.

--- components/ed-modal.js
+++ components/ed-modal.js
@@ -4,13 +4,25 @@ import dynamic from "next/dynamic"
 // self
 import Modal from "./modal"
 
+const initString = "Initializing... One moment please"
+
 const DemoModal = ({ onChange, json, shown = true, close = false }) => {
   if (!shown) return null
 
   const Editor = dynamic(
     () => import("jsoneditor-react").then(({ JsonEditor }) => JsonEditor),
     {
-      loading: () => <p>Initializing, one moment please</p>,
+      loading: () => (
+        <p>
+          {close ? (
+            <button focus onClick={close}>
+              {initString}
+            </button>
+          ) : (
+            initString
+          )}
+        </p>
+      ),
       ssr: false,
     }
   )
