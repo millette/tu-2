911df8e6e3293f200b4b298090d42c3447efa2fe
Robin Millette <robin@millette.info>
2019-11-06 22:21:47 -0500

    Cleanup.

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,14 @@
+// npm
 import Document, { Html, Head, Main, NextScript } from "next/document"
 import { InitializeColorMode } from "theme-ui"
 
 class MyDocument extends Document {
+  /*
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
+  */
 
   render() {
     return (
