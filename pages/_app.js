373dd1b70e52724fbed70561df09933fc647b5c0
Robin Millette <robin@millette.info>
2019-11-08 03:47:01 -0500

    Classless next app.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 /** @jsx jsx */
 
 // npm
-import App from "next/app"
 import Link from "next/link"
 import Head from "next/head"
+import { useState } from "react"
 import { jsx, ThemeProvider, Styled, ColorMode } from "theme-ui"
 
 // self
@@ -39,51 +39,45 @@ const drag = (ev) => {
   }
 }
 
-class MyApp extends App {
-  constructor(props) {
-    super(props)
-    this.state = { theme: {}, shown: false }
-    this.setTheme = (theme) => this.setState({ theme })
-    this.toggle = () => this.setState(({ shown }) => ({ shown: !shown }))
-    this.close = () => this.setState({ shown: false })
-  }
+const MyApp = ({ Component, pageProps }) => {
+  const [elState, setState] = useState({ theme: {}, shown: false })
+  const setTheme = (theme) => setState({ theme })
+  const toggle = () => setState({ shown: !elState.shown })
+  const close = () => setState({ shown: false })
 
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <ThemeProvider components={components} theme={this.state.theme}>
-        <Head>
-          <link rel="stylesheet" href="css/editor.min.css" />
-        </Head>
-        <ColorMode />
-        <Styled.root>
-          <Component {...pageProps} />
-          <div
-            zindex={1000}
-            onMouseDown={drag}
-            style={{
-              background: "white",
-              position: "absolute",
-              overflow: "clip",
-              bottom: 0,
-              right: 0,
-              padding: "0.5rem",
-              border: "0.5rem dotted green",
-            }}
-            id="modal"
-          >
-            {!this.state.shown && <button onClick={this.toggle}>Editor</button>}
-          </div>
-          <EditorModal
-            close={this.close}
-            shown={this.state.shown}
-            json={this.state.theme}
-            onChange={this.setTheme}
-          />
-        </Styled.root>
-      </ThemeProvider>
-    )
-  }
+  return (
+    <ThemeProvider components={components} theme={elState.theme}>
+      <Head>
+        <link rel="stylesheet" href="css/editor.min.css" />
+      </Head>
+      <ColorMode />
+      <Styled.root>
+        <Component {...pageProps} />
+        <div
+          zindex={1000}
+          onMouseDown={drag}
+          style={{
+            background: "white",
+            position: "absolute",
+            overflow: "clip",
+            bottom: 0,
+            right: 0,
+            padding: "0.5rem",
+            border: "0.5rem dotted green",
+          }}
+          id="modal"
+        >
+          {!elState.shown && <button onClick={toggle}>Editor</button>}
+        </div>
+        <EditorModal
+          close={close}
+          shown={elState.shown}
+          json={elState.theme}
+          onChange={setTheme}
+        />
+      </Styled.root>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
